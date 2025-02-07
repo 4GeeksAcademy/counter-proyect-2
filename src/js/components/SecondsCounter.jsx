d21d832ebd5c6f532952a8dda37a8f61af7208e8
adrianallanga <rojas-escobar@hotmail.com>
2025-02-07 21:28:38 +0000

    First commit

---
+++ src/js/components/SecondsCounter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./SecondsCounter.css"
+const SecondsCounter = (props) => {
+    let unidades= Math.floor((props.seconds % 10) / 1);
+    let decenas= Math.floor((props.seconds % 100) / 10);
+    let centenas= Math.floor((props.seconds % 1000) / 100);
+    let unidadesmillar= Math.floor((props.seconds % 10000) / 1000);
+    let decenasmillar= Math.floor((props.seconds % 100000) / 10000);
+    let centenasmillar= Math.floor((props.seconds % 1000000) / 100000);
+    return (
+        <div className="seconds-counter">
+             
+
+        
+    
+    <h1>Seconds </h1>
+   
+<p className="digit">{unidades}</p>
+<p className="digit">{decenas}</p>
+<p className="digit">{centenas}</p>
+<p className="digit">{unidadesmillar}</p>
+<p className="digit">{decenasmillar}</p>
+<p className="digit">{centenasmillar}</p>
+
+<h1> Counter</h1>
+    </div>
+    
+    );
+};
+
+export default SecondsCounter;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,24 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import SecondsCounter from './components/SecondsCounter';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+var intervalID = setInterval(myCallback, 1000);
+let contador=0
+function myCallback() {
+  // Tu código debe ir aquí
+  // Los parámetros son totalmente opcionales
+  console.log("paso un segundo=60");
+  console.log("paso un segundo=1");
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <SecondsCounter seconds={contador}/>
+    </React.StrictMode>,
+  )
+  contador++
+  console.log(contador);
+  console.log(segundos);
+
+}
+
